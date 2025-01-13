89c0278b2d1972e181abe5ad2f9293698f85e20f
ShortLegsFox <mister.dusbin777@gmail.com>
2025-01-13 11:16:37 +0100

    feat: tower targeting enemies

--- KRHNAM-TD/Assets/Scripts/Object/Towers/WaterTower/BasicWaterTower/BasicWaterTower.cs
+++ KRHNAM-TD/Assets/Scripts/Object/Towers/WaterTower/BasicWaterTower/BasicWaterTower.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Abstract;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BasicWaterTower : Tower
 {
+    [SerializeField] private List<EnemyWalk> targetList = new();
+    [SerializeField] private EnemyWalk currentTarget;
+    
     public override void Attack()
     {
         if (isPlaced)
@@ -14,6 +21,47 @@ public class BasicWaterTower : Tower
     
     private void Update()
     {
-        Attack();
+        if (targetList.Count > 0 && currentTarget != null)
+        {
+            Debug.DrawLine(transform.position, currentTarget.transform.position, Color.red);
+        }
+    }
+
+    private void GetCurrentTarget()
+    {
+        if (targetList.Count <= 0)
+        {
+            currentTarget = null;
+            return;
+        }
+        
+        currentTarget = targetList.First();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<EnemyWalk>();
+            if (enemy != null)
+            {
+                targetList.Add(enemy);
+                GetCurrentTarget();
+                Debug.Log("Enemy spotted");
+            }
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<EnemyWalk>();
+            if (enemy != null)
+            {
+                targetList.Remove(enemy);
+                GetCurrentTarget();
+            }
+        }
     }
 }
