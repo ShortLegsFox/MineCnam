f7e2405ae666c85a9d533d59a737bd560eb8764f
Tanguy <pierrontanguy@gmail.com>
2025-01-14 15:15:12 +0100

    Removed element from enemyData

--- KRHNAM-TD/Assets/Scripts/Abstract/Enemy.cs
+++ KRHNAM-TD/Assets/Scripts/Abstract/Enemy.cs
@@ -3,18 +3,19 @@ public abstract class Enemy : Entity
     public GetElementInfos ElementInfos;
     public EnemyData enemyData;
 
+    public Element element;
     public float Hp { get; set; }
 
     public void TakeDamage(Element AttackElement, float AttackDamage)
     {
         // If tower is strong VS enemy
-        if (AttackElement == ElementInfos.GetWeakness(enemyData.Element))
+        if (AttackElement == ElementInfos.GetWeakness(element))
         {
             AttackDamage = ElementInfos.AddStrongDamage(AttackDamage);
         }
 
         // If tower is weak vs enemy
-        if (AttackElement == ElementInfos.GetStrength(enemyData.Element))
+        if (AttackElement == ElementInfos.GetStrength(element))
         {
             AttackDamage = ElementInfos.RemoveWeakDamage(AttackDamage);
         }
--- KRHNAM-TD/Assets/Scripts/Object/Enemy/EnemyData.cs
+++ KRHNAM-TD/Assets/Scripts/Object/Enemy/EnemyData.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-    public Element Element;
     public GameObject Prefab;
     public int MaxHp;
     public int Damage;
