fa907f19a9b9ce9269d42452cd32922f5c2b908b
Léo HOTZ <cdf.leo.hotz@gmail.com>
2025-01-28 09:18:32 +0100

    Feat(Healthbar): Healthbar follow camera

--- KRHNAM-TD/Assets/HealthBar.cs
+++ KRHNAM-TD/Assets/HealthBar.cs
@@ -3,16 +3,44 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider;
+    private Slider slider;
+    private int currentHealth;
+    public int maxHealth;
+    public Gradient gradient;
+    public Image fill;
+
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(10);
+        }
+    }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        SetHealth(currentHealth);
     }
 }
