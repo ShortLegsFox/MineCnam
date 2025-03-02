8a9aaf486b41303e5f17455fb3838bfec2ee256d
ShortLegsFox <mister.dusbin777@gmail.com>
2025-03-02 21:58:04 +0100

    feat: parasite effect work, needs improvment

--- KRHNAM-TD/Assets/Scripts/Abstract/Enemy.cs
+++ KRHNAM-TD/Assets/Scripts/Abstract/Enemy.cs
@@ -14,7 +14,7 @@ public abstract class Enemy : Entity
     public float Hp { get; set; }
     public int currentSpeed { get; set; }
 
-    private GameObject target;
+    public GameObject target;
     private bool canAttack = true;
     private Animator animator;
     
@@ -37,9 +37,7 @@ public abstract class Enemy : Entity
     public void Update()
     {
         IsTheTargetInRange();
-        
         ApplyEffects();
-
     }
 
     public bool TakeDamage(Element AttackElement, float AttackDamage, bool fromEffect = false, EffectData effectData = null)
@@ -101,8 +99,7 @@ public abstract class Enemy : Entity
             }
         }
     }
-
-
+    
     private float GetDistanceFromTarget()
     {
         // Get distance from target
@@ -134,6 +131,13 @@ public abstract class Enemy : Entity
                 castle.TakeDamage(enemyData.Damage);
                 StartCoroutine(AttackCooldown());
             }
+
+            Enemy enemy = target.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(element, enemyData.Damage, true);
+                StartCoroutine(AttackCooldown());
+            }
         }
     }
 
@@ -143,7 +147,27 @@ public abstract class Enemy : Entity
         yield return new WaitForSeconds(enemyData.TimeBetweenAttacks);
         canAttack = true;
     }
+    
+    public Enemy FindClosestEnemy()
+    {
+        Enemy[] allEnemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None);
+        Enemy closestEnemy = null;
+        float minDistance = Mathf.Infinity;
 
+        foreach (Enemy e in allEnemies)
+        {
+            if (e == this) continue;
+
+            float distance = Vector3.Distance(transform.position, e.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestEnemy = e;
+            }
+        }
+        
+        return closestEnemy;
+    }
 
     private bool OnDeath()
     {
---
+++ KRHNAM-TD/Assets/Scripts/Object/Effect/Parasite/Parasite.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Parasite : Effect
+{
+    private EffectData effectData;
+    private Enemy parasitizedTarget;
+    
+    public Parasite(float duration, EffectData effectData) : base(duration)
+    {
+        this.duration = duration;
+        this.effectData = effectData;
+    }
+    
+    public override bool Apply(Enemy enemy)
+    {
+        elapsedTime += Time.deltaTime;
+        
+        if (elapsedTime >= duration)
+        {
+            RemoveEffect(enemy);
+            return false;
+        }
+
+        EnemyTypeAbs enemyMovement = enemy.GetComponent<EnemyTypeAbs>();
+        
+        if (parasitizedTarget == null)
+        {
+            parasitizedTarget = enemy.FindClosestEnemy();
+            if (parasitizedTarget != null)
+            {
+                enemy.target = parasitizedTarget.gameObject;
+                enemyMovement.castle = parasitizedTarget.gameObject;
+                enemyMovement.agent.SetDestination(enemy.target.transform.position);
+            }
+        }
+
+        enemy.debuffIcon.sprite = this.effectData.effectIcon;
+        enemy.debuffIcon.enabled = true;
+
+        return true;
+    }
+    
+    public void RemoveEffect(Enemy enemy)
+    {
+        EnemyTypeAbs enemyMovement = enemy.GetComponent<EnemyTypeAbs>();
+        
+        enemy.target = GameManager.Instance.Castle;
+        enemyMovement.castle = GameManager.Instance.Castle;
+        enemyMovement.agent.SetDestination(enemy.target.transform.position);
+
+        // Réinitialiser l'état du parasite
+        parasitizedTarget = null;
+        enemy.debuffIcon.enabled = false;
+    }
+    
+    public override void Refresh()
+    {
+        elapsedTime = 0;
+    }
+}
